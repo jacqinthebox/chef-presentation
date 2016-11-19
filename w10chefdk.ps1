df0e8407abb5df42d50d1a578e67156bcf8a1cc1
jacqueline <jacqueline@vagrantwin10>
2016-11-19 02:40:27 -0800

    removed choco install windowsazurepowershellw

--- w10chefdk.ps1
+++ w10chefdk.ps1
@@ -9,14 +9,18 @@ New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Expl
 
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 Invoke-Webrequest https://raw.githubusercontent.com/mmillar-bolis/ps-motd/master/Get-MOTD.ps1 -outfile $env:USERPROFILE\Documents\WindowsPowerShell\Get-MOTD.ps1
+
 Install-Module Posh-Git -Force
+Install-Module AzureRM -Force
+Install-Module Azure -Force
+
 Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/chef-presentation/master/Microsoft.PowerShell_profile.ps1 -OutFile C:\Users\vagrant\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/chef-presentation/master/Microsoft.PowerShellISE_profile.ps1 -OutFile C:\Users\vagrant\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1
+
 choco install chefdk -force -yes
 choco install visualstudiocode -force -yes
 choco install googlechrome -force -yes
 choco install git -force -yes
 choco install conemu -force -yes
-choco install windowsazurepowershell -force -yes
 
 
