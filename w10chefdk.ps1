778fb0cb38194a7d41c69f841068c8b2f884de0a
vagrant <vagrant>
2016-11-19 00:01:44 -0800

    profiles

--- w10chefdk.ps1
+++ w10chefdk.ps1
@@ -5,5 +5,10 @@ Set-ExecutionPolicy Bypass
 New-Item -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Force
 New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType dword -Value 1
 
-Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-choco install chefdk visualstudiocode googlechrome windowsazurepowershell -force -yes
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Invoke-Webrequest https://raw.githubusercontent.com/mmillar-bolis/ps-motd/master/Get-MOTD.ps1 -outfile $env:USERPROFILE\Documents\WindowsPowerShell\Get-MOTD.ps1
+Install-Module Posh-Git -Force
+
+choco install chefdk visualstudiocode googlechrome windowsazurepowershell conemu git -force -yes
+
+
