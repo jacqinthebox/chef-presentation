249dc1f1424cfc923464e17183f7acd60b4a2ea0
Jacqueline <jacq@in-the-box.nl>
2016-11-27 13:39:39 +0100

    Create win10devbox.ps1

---
+++ 20161122-chef-expertslive/win10devbox.ps1
@@ -0,0 +1,20 @@
+# Base Win10 development box
+
+Set-ExecutionPolicy Bypass
+
+New-Item -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Force
+New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType dword -Value 1
+
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Install-PackageProvider -Name Nuget -Force
+Invoke-Webrequest https://raw.githubusercontent.com/mmillar-bolis/ps-motd/master/Get-MOTD.ps1 -outfile $env:USERPROFILE\Documents\WindowsPowerShell\Get-MOTD.ps1
+
+Install-Module Posh-Git -Force
+
+Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/presentations/chef-presentation/master/Microsoft.PowerShell_profile.ps1 -OutFile C:\Users\vagrant\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/presentations/chef-presentation/master/Microsoft.PowerShellISE_profile.ps1 -OutFile C:\Users\vagrant\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1
+
+choco install visualstudiocode -force -yes
+choco install googlechrome -force -yes
+choco install git -force -yes
+choco install conemu -force -yes
