2eb254c920d1fde2ae40e84292f5e6eaee1154e2
Jacqueline <jacqueline@moonstreet.nl>
2016-12-01 15:51:41 +0100

    ready I think

--- 20161202-dockerdemo/demo.sh
+++ 20161202-dockerdemo/demo.sh
@@ -2,8 +2,8 @@ docker run -d -p 8080:80 nginx
 
 # diverse commands
 #docker -d #daemon -p #port -t -i  
-docker inspect
-docker ps
+docker inspect containername grep
+docker ps -a -q
 docker stop 
 docker images
 docker rm $(docker ps -a -q)
@@ -11,14 +11,15 @@ docker rmi $(docker images -q)
 
 ## mount a volume
 # docker preferences!
-docker run -d -p 8081:80 -v /Users/jacqueline/code/temp/html:/usr/share/nginx/html nginx
+#mkdir html
+docker run -d -p 8081:80 -v $(pwd)/html:/usr/share/nginx/html nginx
 
 
-## copy a folder and build a new computer
+## copy a folder and build a new container
 FROM nginx
 COPY html /usr/share/nginx/html
 
-#docker build -t newimage .
+docker build -t newimage .
 docker run -p 8082:80 newimage
 
 ## terminal access
@@ -49,7 +50,10 @@ docker-compose logs
 docker images
 
 ## Azure
-docker-machine create -d azure --azure-ssh-user ops --azure-subscription-id thw45kukldsd38  --azure-open-port 80 docker-platform2
-docker-machine.exe env --shell powershell docker-platform2 | Invoke-Expression
+azure login
+azure account list
+docker-machine create -d azure --azure-ssh-user ops --azure-subscription-id 6b9f4039-bac1-4573-ac35-068a1073ed38  --azure-open-port 80 docker-platform3
+#docker-machine.exe env --shell powershell docker-platform2 | Invoke-Expression
+docker eval "$(docker-machine env docker-platform3)"
 docker-machine active
 docker-machine list
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Demo material for my presentations
+# Demo material for presentations
 
 ## Install a Windows 10 Base Dev Box
 This installs a base box with Git, Google Chrome, ConEmu and Visual Studio Code.
@@ -18,3 +18,14 @@ vagrant up --provider parallels
 
 (Unfortunately I do not have an Hyper-V box yet)
 
+## Presentation tricks
+Clean up the line: You can use Ctrl+U to clear up to the beginning.
+Clean up the line: Ctrl+A Ctrl+K to wipe the current line in the terminal
+Cancel the current command/line: Ctrl+C.
+Recall the deleted command: Ctrl+Y (then Alt+Y)
+Go at the beginning of the line: Ctrl+A
+Go at the end of the line: Ctrl+E
+Remove the forward words for example, if you are middle of the command: Ctrl+K
+Remove characters on the left, until the beginning of the word: Ctrl+W
+To clear your entire command prompt: Ctrl + L
+Toggle between the start of line and current cursor position: Ctrl + XX
