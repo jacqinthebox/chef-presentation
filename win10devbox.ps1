251e9050a1e76b60d957bedc29c527e83890e5e5
Jacqueline <jacq@in-the-box.nl>
2017-02-18 10:11:33 +0100

    Update win10devbox.ps1

--- win10devbox.ps1
+++ win10devbox.ps1
@@ -7,17 +7,14 @@ New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Expl
 
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 Install-PackageProvider -Name Nuget -Force
-Invoke-Webrequest https://raw.githubusercontent.com/mmillar-bolis/ps-motd/master/Get-MOTD.ps1 -outfile $env:USERPROFILE\Documents\WindowsPowerShell\Get-MOTD.ps1
 
 Install-Module Posh-Git -Force
+Install-Module ISESteroids -Scope CurrentUser -Force
+Install-Module PsISEProjectExplorer -Force
+Import-Module PsISEProjectExplorer
 
 Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/presentations/master/Microsoft.PowerShell_profile.ps1 -OutFile $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/presentations/master/Microsoft.PowerShellISE_profile.ps1 -OutFile $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1
-
-Install-Module ISESteroids -Scope CurrentUser
-Install-Module PsISEProjectExplorer
-Import-Module PsISEProjectExplorer
-Add-PsISEProjectExplorerToIseProfile
 
 choco install visualstudiocode -force -yes
 choco install googlechrome -force -yes
