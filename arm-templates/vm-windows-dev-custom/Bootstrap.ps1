cad3705d0f2039caa1b50e1bfcaf4391a0b2dfb4
Jacqueline <jacq@in-the-box.nl>
2018-02-14 14:08:18 +0100

    Update Bootstrap.ps1

--- arm-templates/vm-windows-dev-custom/Bootstrap.ps1
+++ arm-templates/vm-windows-dev-custom/Bootstrap.ps1
@@ -5,8 +5,13 @@ Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-E
 Install-PackageProvider -Name Nuget -Force
 Install-Module Posh-Git -Force
 Install-Module ISESteroids -Scope CurrentUser -Force
+
 choco install visualstudiocode -force -yes
 choco install firefox -force -yes
 choco install git -force -yes
 choco install conemu -force -yes
 choco install notepadplusplus -force -yes
+choco install visualstudio2017enterprise -force -yes
+choco install sql-server-management-studio -force -yes
+choco install sql-server-express -force -yes
+choco install resharper -force -yes
